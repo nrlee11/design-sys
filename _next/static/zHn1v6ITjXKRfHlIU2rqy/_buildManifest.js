d36b82b1d994df7a60c0e790c9bdba1d0310c239
naralee <nrlee@leadit.co.kr>
2026-03-25 15:40:49 +0900

    Updates

--- _next/static/zHn1v6ITjXKRfHlIU2rqy/_buildManifest.js
+++ _next/static/zHn1v6ITjXKRfHlIU2rqy/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/design-sys//_next/:path+",
-        "destination": "/design-sys/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
